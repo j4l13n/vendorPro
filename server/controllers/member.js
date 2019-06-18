7071d4bc9ec95dd2b2df8e2bc66465112a2da81a
Ndahimana Dominique <ndahimanadominique@Ndahimanas-MacBook-Pro.local>
2019-06-18 16:45:51 +0200

    Create a function for register member

--- server/controllers/member.js
+++ server/controllers/member.js
@@ -19,7 +19,39 @@ class Members {
             }));
     }
 
-    static registerMember(req, res) {}
+    static registerMember(req, res) {
+        const memberData = {
+            firstname: req.member.firstname,
+            lastname: req.member.lastname,
+            email: req.member.email.toLowerCase(),
+            type: req.member.type,
+            isactive: true,
+            owner:req.user.id
+        };
+
+        Member.findOne({
+            where: {
+                email: req.member.email,
+            }
+        })
+        .then(member => {
+            if (member) {
+                res.status(409).json({ error: 'Member already recorded!' })
+            }
+            else{
+                Member.create(memberData)
+                .then(member => {
+                    return res.status(201).json({
+                        message:"Member Created successfully",
+                        data: memberData
+                    })
+                })
+                .catch(err => {
+                    res.status(400).send('error' + err);
+                })
+            }
+        })
+    }
 }
 
 export default Members;
